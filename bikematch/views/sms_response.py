1cccaee004672741dedf881f5281f89fdd50a9eb
Bill Leddy <bill@williesworkshop.net>
2020-11-17 08:06:17 -0800

    better response template

--- bikematch/views/sms_response.py
+++ bikematch/views/sms_response.py
@@ -9,22 +9,37 @@ mod = Blueprint('sms_response',__name__, template_folder='templates/sms_response
 
 
 @mod.route('/',methods=['GET','POST',])
-@mod.route('/<path:path>',methods=['GET','POST',])
-@mod.route('/<path:path>/',methods=['GET','POST',])
-def handle_request(path=''):
+def handle_request():
     """Handle sms requests from twilio"""
-    
-    # is there a request.form?
-    if request.form:
-        mess = "form= " +str(request.form)
-    elif request.args:
-        mess = "args: " + str(request.args)
-    else:
-        mess = "no form or args"
             
-    resp = TextResponse()
-    resp.create_message(mess)
+    resp = TextResponse(request)
+    # resp now contains these properties:
+    #   resp.body = the text sent to us
+    #   resp.from_number = the phone number that messaged us
+    #   resp.to_number = the phone number we used to message the caller
+    # if for some reason, the properties were not set, they will
+    # contain the empty string
     
-    return Response(str(resp.message),
-                    mimetype="text/xml",
-                    )
+    # At this point, create your message text
+    mess = "Hello World!"
+    if resp.to_number or resp.from_number:
+        mess = "Hello {to_} from {from_}".format(to_=resp.to_number,from_=resp.from_number)
+        
+    #  and pass it to resp
+    resp.create_message(mess)
+    # you can test resp.success and resp.result_text to see how it went
+    
+    # you can now attach media, if you like
+    url = url_for('static',filename='images/header-logo.jpg') # a site relative path 
+    # or
+    # url = "http://{host_name}/{media_path}" # an absolut url
+    
+    # and attach it to the message
+    resp.attach_media(url)
+    # as many as you want...
+    url = url_for('static',filename='favicon.png') # a site relative path 
+    resp.attach_media(url)
+    
+    #finally, send the response
+    return resp.render_response()
+    
